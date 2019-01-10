715a8a825a67f251a90615d8d18ee03779e8ea72
ahmadthoyib <thoyibghofur16@gmail.com>
2019-01-10 20:26:43 +0700

    object

---
+++ js.js
@@ -0,0 +1,43 @@
+/*function sayHello(){
+    for (var i = 0; i < 5; i++){
+         console.log(i);
+    }
+}
+sayHello() */ 
+
+// const person = {
+//     name:'Bagio',
+//     height : 168,
+//     age : 16
+// }
+// console.log(person)
+
+// const square = function(number){
+//     return number * number;
+// };
+
+//const square = number => number * number;
+//console.log(square(6));
+
+/*let name = "thoyib";
+console.log(name);
+
+/*let fistname = "ahmad";
+let lastname = "thoyib";*/
+
+/*let name = "thoyib";
+let age  = "18";
+let isapproved = "false";
+let firstname = "underfined";
+let selectedcolor = "null";*/
+
+let person ={
+    name : 'thoyib',
+    age  : 18,
+};
+person.name = 'ahmad';
+
+let slection = 'name';
+person[Selection]= 'ghofur';
+
+console.log(person.name);
