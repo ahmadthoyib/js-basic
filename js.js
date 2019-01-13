4e80144dff7f344cf5bc98dbaed5dd13c9559767
ahmadthoyib <thoyibghofur16@gmail.com>
2019-01-13 22:31:16 +0700

    asignmant operators

--- js.js
+++ js.js
@@ -58,19 +58,27 @@ console.log(square(5))*/
 
 //operators
 
-let x = 10
+/*let x = 10
 let y = 5
 
-/*console.log(x + y)
+console.log(x + y)
 console.log(x - y)
 console.log(x * y)
 console.log(x / y)
 console.log(x % y)
-console.log(x ** y)*/
+console.log(x ** y)
 
 //increment (++)
 console.log(x++)
 console.log(x)
 
 //derement (--)
-console.log(--x)
+console.log(--x)*/
+
+let x =  10;
+
+x = x + 5;
+x += 5; 
+
+x = x * 3;
+x *= 3
