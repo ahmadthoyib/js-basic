c59c9a942485bb02b0e52d1f8546df8d027eacfd
ahmadthoyib <thoyibghofur16@gmail.com>
2019-01-15 00:24:20 +0700

    do while

--- js.js
+++ js.js
@@ -209,11 +209,32 @@ else console.log('unknow user');*/
 
  //4. while
 
- for(let i = 0; i <= 5; i++){
+ /*for(let i = 0; i <= 5; i++){
     if (i % 2 !== 0)console.log(i)
 }
 let i = 0;
 while(i <= 5){
     if (i % 2 !== 0)console.log(i);
     i++;
-}
+}*/
+
+//5. do while
+
+//for(let i = 0; i <= 5; i++){
+    //if (i % 2 !== 0)console.log(i)
+//}
+/*let i = 9;
+while(i <= 5){
+    if (i % 2 !== 0)console.log(i);
+    i++;
+}*/
+//do whhile
+
+let i = 9;
+do{
+    if (i % 2 !== 0)console.log(i);
+    i++;
+}while (i <= 5);
+
+
+
