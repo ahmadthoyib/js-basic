092edee6af0bb5fbd01a59062cf475057db5c73e
ahmadthoyib <thoyibghofur16@gmail.com>
2019-01-15 22:51:55 +0700

    fizzbuzz

--- js.js
+++ js.js
@@ -291,8 +291,28 @@ for (let color of colors)
  }*/
 
  //11.landscape and potrait
- console.log(isLandscape(300, 600));
+ /*console.log(isLandscape(300, 600));
 
  function isLandscape(width, height){
      return(width > height);
- }
+ }*/
+
+ //12.fizz buz
+ const output = fizzBuzz(false);
+    console.log(output);
+
+function fizzBuzz(input){
+    if (typeof input !== 'number')
+    return NaN;
+
+    if ((input % 3 === 0) && (input % 5 === 0))
+    return 'fizzBuzz';
+
+    if (input  % 3 === 0)
+    return 'fizz';
+
+    if (input % 5 === 0)
+    return 'Buzz';
+
+    return input;
+}
